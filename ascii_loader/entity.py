2351cdc78140c271f7e052a9bdefbca9459691f7
michaelallan <mb.allan@ymail.com>
2023-06-04 01:11:29 -0700

    Made entity's x and y public to get and set

--- ascii_loader/entity.py
+++ ascii_loader/entity.py
@@ -6,8 +6,8 @@ class Entity(ABC):
     An object that is loaded from a map
     """
 
-    _x: int
-    _y: int
+    x: int
+    y: int
 
     def __init__(self, x: int, y: int):
         """
@@ -16,13 +16,5 @@ class Entity(ABC):
         :param x: The entity's x position in a map
         :param y: The entity's y position in a map
         """
-        self._x = x
-        self._y = y
-
-    @property
-    def x(self) -> int:
-        return self._x
-
-    @property
-    def y(self) -> int:
-        return self._y
+        self.x = x
+        self.y = y
