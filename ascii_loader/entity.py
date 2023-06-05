b4ed15143f95a3d383bbe0024fdfe0ccaf679ff0
401asfg <mb.allan@ymail.com>
2023-06-05 16:54:41 -0700

    Made Entity's x and y setters private again

--- ascii_loader/entity.py
+++ ascii_loader/entity.py
@@ -6,8 +6,8 @@ class Entity(ABC):
     An object that is loaded from a map
     """
 
-    x: int
-    y: int
+    _x: int
+    _y: int
 
     def __init__(self, x: int, y: int):
         """
@@ -16,5 +16,13 @@ class Entity(ABC):
         :param x: The entity's x position in a map
         :param y: The entity's y position in a map
         """
-        self.x = x
-        self.y = y
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self) -> int:
+        return self._x
+
+    @property
+    def y(self) -> int:
+        return self._y
